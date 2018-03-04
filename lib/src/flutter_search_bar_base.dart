6bf014bc1d7ec2b9c3c3ebe31ae203d68f20b477
Spencer <spencer@frozor.io>
2018-03-03 20:55:15 -0500

    Add clear button to search bar

--- lib/src/flutter_search_bar_base.dart
+++ lib/src/flutter_search_bar_base.dart
@@ -11,23 +11,40 @@ typedef void SetStateCallback(void fn());
 class SearchBar {
   /// Whether the search should take place "in the existing search bar", meaning whether it has the same background or a flipped one. Defaults to true.
   final bool inBar;
+
   /// Whether the back button should be colored, if this is false the back button will be Colors.grey.shade400
   final bool colorBackButton;
+
   /// Whether or not the search bar should close on submit. Defaults to true.
   final bool closeOnSubmit;
+
   /// What the hintText on the search bar should be. Defaults to 'Search'.
   final String hintText;
+
+  /// Whether the text field should be cleared when it is submitted
+  final bool clearOnSubmit;
+
   /// A callback which should return an AppBar that is displayed until search is started. One of the actions in this AppBar should be a search button which you obtain from SearchBar.getSearchAction(). This will be called every time search is ended, etc. (like a build method on a widget)
   final AppBarCallback buildDefaultAppBar;
+
   /// A void callback which takes a string as an argument, this is fired every time the search is submitted. Do what you want with the result.
   final TextFieldSubmitCallback onSubmitted;
+
   /// Since this should be inside of a State class, just pass setState to this.
   final SetStateCallback setState;
+
+  /// Whether or not the search bar should add a clear input button, defaults to true.
+  final bool showClearButton;
+
   /// The controller to be used in the textField.
-  final TextEditingController controller;
+  TextEditingController controller;
 
   /// Whether search is currently active.
   bool _isSearching = false;
+
+  /// Whether the clear button should be active (fully colored) or inactive (greyed out)
+  bool _clearActive = false;
+
   /// The last built default AppBar used for colors and such.
   AppBar _defaultAppBar;
 
@@ -39,8 +56,45 @@ class SearchBar {
     this.hintText = 'Search',
     this.inBar = true,
     this.colorBackButton = true,
-    this.closeOnSubmit = true
-  });
+    this.closeOnSubmit = true,
+    this.clearOnSubmit = true,
+    this.showClearButton = true
+  }) {
+    if (this.controller == null) {
+      this.controller = new TextEditingController();
+    }
+
+    // Don't waste resources on listeners for the text controller if the dev
+    // doesn't want a clear button anyways in the search bar
+    if (!this.showClearButton) {
+      return;
+    }
+
+    this.controller.addListener(() {
+      print('is empty: ' + controller.text.isEmpty.toString());
+      print('value of clearActive: ' + _clearActive.toString());
+
+      if (this.controller.text.isEmpty) {
+        // If clear is already disabled, don't disable it
+        if (_clearActive) {
+          setState(() {
+            print('Setting clear to inactive');
+            _clearActive = false;
+          });
+        }
+
+        return;
+      }
+
+      // If clear is already enabled, don't enable it
+      if (!_clearActive) {
+        setState(() {
+          print('Setting clear to active');
+          _clearActive = true;
+        });
+      }
+    });
+  }
 
   /// Initializes the search bar.
   ///
@@ -77,24 +131,30 @@ class SearchBar {
   AppBar buildSearchBar(BuildContext context) {
     ThemeData theme = Theme.of(context);
 
+    Color barColor = inBar ? _defaultAppBar.backgroundColor : theme.canvasColor;
+
+    // Don't provide a color (make it white) if it's in the bar, otherwise color it or set it to grey.
+    Color buttonColor = inBar ? null : (colorBackButton ? _defaultAppBar.backgroundColor ?? theme.primaryColor ?? Colors.grey.shade400 : Colors.grey.shade400);
+    Color buttonDisabledColor = inBar ? new Color.fromRGBO(255, 255, 255, 0.25) : Colors.grey.shade300;
+
+    Color textColor = inBar ? Colors.white70 : Colors.black54;
+
     return new AppBar(
       leading: new BackButton(
-        // Don't provide a color (make it white) if it's in the bar, otherwise color it or set it to grey.
-        color: inBar ? null : (colorBackButton ? _defaultAppBar.backgroundColor ?? theme.primaryColor ?? Colors.grey.shade400 : Colors.grey.shade400)
-
+        color: buttonColor
       ),
-      backgroundColor: inBar ? _defaultAppBar.backgroundColor : theme.canvasColor,
+      backgroundColor: barColor,
       title: new TextField(
         key: new Key('SearchBarTextField'),
         keyboardType: TextInputType.text,
         style: new TextStyle(
-          color: inBar ? Colors.white70 : Colors.black54,
+          color: textColor,
           fontSize: 16.0
         ),
         decoration: new InputDecoration(
           hintText: hintText,
           hintStyle: new TextStyle(
-            color: inBar ? Colors.white70 : Colors.black54,
+            color: textColor,
             fontSize: 16.0
           ),
           border: null
@@ -104,17 +164,22 @@ class SearchBar {
             await Navigator.maybePop(context);
           }
 
+          if (clearOnSubmit) {
+            controller.clear();
+          }
+
           onSubmitted(val);
         },
         autofocus: true,
         controller: controller,
       ),
-      /*actions: <Widget>[
+      actions: !showClearButton ? null : <Widget>[
+        // Show an icon if clear is not active, so there's no ripple on tap
         new IconButton(
-            icon: new Icon(Icons.send),
-            onPressed: null
-        )
-      ]*/
+            icon: new Icon(Icons.clear, color: _clearActive ? buttonColor : buttonDisabledColor),
+            disabledColor: buttonDisabledColor,
+            onPressed: !_clearActive ? null : () { controller.clear(); })
+      ],
     );
   }
 
